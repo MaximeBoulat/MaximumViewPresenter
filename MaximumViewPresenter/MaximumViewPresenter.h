a9d1a241680cfa4e19d61c516c1d45b608ca40db
Maxime Boulat <mboulat@maxime-boulat.attlocal.net>
2020-01-25 17:20:54 -0800

    Initial commit

---
+++ MaximumViewPresenter/MaximumViewPresenter.h
@@ -0,0 +1,19 @@
+//
+//  MaximumViewPresenter.h
+//  MaximumViewPresenter
+//
+//  Created by Maxime Boulat on 1/25/20.
+//  Copyright © 2020 Maxime Boulat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MaximumViewPresenter.
+FOUNDATION_EXPORT double MaximumViewPresenterVersionNumber;
+
+//! Project version string for MaximumViewPresenter.
+FOUNDATION_EXPORT const unsigned char MaximumViewPresenterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaximumViewPresenter/PublicHeader.h>
+
+
